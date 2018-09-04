70d4fb2e0bcf4193164726a08b66a0aabe5cc090
sklee <hadesleeq@gmail.com>
2018-09-04 10:35:01 +0900

    raw_input 입력

--- chapter2/sec02.py
+++ chapter2/sec02.py
@@ -1,7 +1,13 @@
 # -*- coding: utf-8 -*-
 # UTF-8 encoding when using korean
+from pip._vendor.distlib.compat import raw_input
 
 age = input("나이를 입력해 주세요: ")
 #=> 20 입력
 print(age)
 #=> 20 출력
+
+name = raw_input("이름을 입력해 주세요 : ")
+#=>홍길동 입력
+print(name)
+#=>홍길동 출력
