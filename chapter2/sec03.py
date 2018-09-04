8bfc14a6d8b4b12fd4cbb96f2c455f76f3d5fc98
sklee <hadesleeq@gmail.com>
2018-09-04 10:37:33 +0900

    input : 숫자 raw_input 문자

---
+++ chapter2/sec03.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+# UTF-8 encoding when using korean
+from pip._vendor.distlib.compat import raw_input
+
+age = input("자신의 나이를 입력해 주세요 : ")
+city = raw_input("살고 있는 도시를 입력해 주세요 : ")
+print(age)
+print(city)
