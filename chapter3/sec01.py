d6c7d081b37d591d3e26696ab301e57e7762d9a9
sklee <hadesleeq@gmail.com>
2018-09-05 09:53:39 +0900

    숫자형과 연산자

---
+++ chapter3/sec01.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+# UTF-8 encoding when using korean
+
+x = 1
+x = -2
+x = 0
+
+x = 23.98
+x = -12.22
+
+a = 1 + 2j
+a.real
+#=>실수 부분 1.0 리턴
+
+a.imag
+#=>허수 부분 2.0 리턴
+
+a.conjugate()
+#=>컬레 복소수 1-2j 리턴
+
+abs(a)
+#=>복소수 절대값 2.23606797749979 리턴
