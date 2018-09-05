abfe592866b5d5d32907fdb0136234ac068b38c3
sklee <hadesleeq@gmail.com>
2018-09-05 10:08:01 +0900

    숫자형과 연산자

---
+++ chapter3/sec02.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+# UTF-8 encoding when using korean
+a = 5
+b = 4
+c = -2
+
+result1 = a / b
+result2 = a / c
+remainder = a % b
+
+print(result1)
+print(result2)
+print(remainder)
