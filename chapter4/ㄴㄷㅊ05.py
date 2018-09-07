fa21bae3519769f34bf649f18a61a26555322297
sklee <hadesleeq@gmail.com>
2018-09-07 10:29:03 +0900

    문자열 포매팅

--- chapter4/ㄴㄷㅊ05.py
+++ chapter4/ㄴㄷㅊ05.py
@@ -1,9 +1,17 @@
 # -*- coding: utf-8 -*-
 # UTF-8 encoding when using korean
 
-a = "저는 %d 살 입니다. " %20
+a = "저는 %d 살 입니다. " % 20
 print(a)
 
-a = "Hello, %s" %'there!'
+a = "Hello, %s" % 'there!'
 print(a)
 
+a = "python!"
+b = "I love %s" % a
+print(b)
+
+a = "사과"
+b = "바나나"
+c = "나는 %s와 %s를 좋아해" % (a, b)
+print(c)
