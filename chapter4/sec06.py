2411649ba6e403c052123611589bd460b631b68e
sklee <hadesleeq@gmail.com>
2018-09-07 11:17:27 +0900

    문자열 함수

--- chapter4/sec06.py
+++ chapter4/sec06.py
@@ -6,5 +6,24 @@ print(a.count('o'))
 print(a.find('v'))
 print(a.index('p'))
 
-print(a.find('x'))
+print(a.find('x'))  # find는 찾는 문자열이 없으면 -1
 # print(a.index('x')) index는 문자열이 없으면 오류
+
+a = " ".join("abcde")
+print(a)
+b = "1".join("abcde")
+print(b)
+c = "python".upper()
+print(c)
+d = "HELLO".lower()
+print(d)
+e = "blue".replace('b', 'g')
+print(e)
+
+f = "Hello world I'm python".split(" ")
+print(f)
+
+g = "python".split('th')
+print(g)
+
+
