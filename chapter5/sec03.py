5060f96f8aca5aba7e2261075e044c8851284b7d
hadesyi <leesk55@gmail.com>
2018-09-09 02:18:20 +0900

    리스트 슬라이싱

--- chapter5/sec03.py
+++ chapter5/sec03.py
@@ -10,3 +10,16 @@ my_list = [1, 2, 3, 4, 5]
 print(my_list[0])
 print(my_list[3])
 print(my_list[0] + my_list[3])
+
+my_list = [1, 2, ['a', 'b'], 'python']
+print(my_list[2])
+print(my_list[2][0])
+print(my_list[2][1])
+
+a = [1, 2, 3, 4, 5]
+print(a[1:4])
+print(a[:3])
+print(a[2:])
+
+a[2] = 'hi'
+print(a)
