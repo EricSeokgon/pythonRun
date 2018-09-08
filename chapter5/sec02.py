1a2b4638b98dd3e1cbf972522b191dcca9840645
hadesyi <leesk55@gmail.com>
2018-09-09 02:13:47 +0900

    리스트 만들기

---
+++ chapter5/sec02.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+# UTF-8 encoding when using korean
+oddnumber = [1, 3, 5, 7, 9]
+cafes = ['starbucks', 'ediya', 'cafebene', 'yogerpresso']
+A = [1, 5, 'A', 'CC', 'B']
+listinList = [[1, 3, 5, 7], cafes, oddnumber, 1, 3, 'Abc']
+
+print(oddnumber)
+
+print(cafes)
+
+print(listinList)
