e84a6c488ab670fe52c43e4bb2aaabd2844c8c21
sklee <hadesleeq@gmail.com>
2018-09-10 10:20:59 +0900

    사전자료형의 key/value

--- chapter6/sec01.py
+++ chapter6/sec01.py
@@ -4,3 +4,10 @@
 my_dic = {'apple': '사과', 'bird': '새', 'class': '수업'}
 print(my_dic['apple'])
 print(my_dic['bird'])
+
+a = {'apple': '사과', 'bird': '새'}
+a['class'] = '수업'
+print(a)
+
+del a['apple']
+print(a)
