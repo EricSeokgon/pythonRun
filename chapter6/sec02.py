2ada10e49ef7d6abee6c8d5530a0aa1ebe746f6f
sklee <hadesleeq@gmail.com>
2018-09-10 11:02:45 +0900

    사전 자료형의 함수

--- chapter6/sec02.py
+++ chapter6/sec02.py
@@ -3,7 +3,17 @@
 
 my_dic = {'name': 'abc', 'year': 2015, 'age': 20}
 
-print(my_dic.key())
+print(my_dic.keys())
 print(my_dic.values())
 
 print(my_dic.items())
+print(my_dic.clear())
+
+print(my_dic.get('name'))
+print(my_dic.get('age'))
+print(my_dic.get('hello'))
+# print(my_dic.['hello'])
+
+my_dic = {'name': 'abc', 'year': 2015, 'age': 20}
+print('name' in my_dic)
+print('hello' in my_dic)
