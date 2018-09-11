3ee2c3ad05c203eaac3aa827b27cba2f585a4bde
sklee <hadesleeq@gmail.com>
2018-09-11 16:50:07 +0900

    if를 이용한 조건문

---
+++ chapter9/sec01.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+# UTF-8 encoding when using korean
+
+n = input("정수 입력 : ")
+if (n < 0):
+    print("입력 값은 0보다 작다.")
+elif (n > 0):
+    print("입력 값은 0보다 크다.")
+else:
+    print("입력 값은 0이다.")
