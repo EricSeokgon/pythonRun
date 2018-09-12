5dc5a73cb4efafd1fabf23b25a3c7e93994d406d
sklee <hadesleeq@gmail.com>
2018-09-12 09:41:20 +0900

    if를 이용한 조건문

--- chapter9/sec01.py
+++ chapter9/sec01.py
@@ -8,3 +8,23 @@ elif (n > 0):
     print("입력 값은 0보다 크다.")
 else:
     print("입력 값은 0이다.")
+
+x = 1
+y = 2
+
+if (x > y):
+elif (x < y):
+
+elif (x == y):
+elif (x != y):
+
+elif (x >= y):
+elif (x <= y):
+
+money = 2000
+card = -9999999
+
+if(money>=6000 or card):
+    print("택시를 타라")
+else:
+    print("걸어가라")
