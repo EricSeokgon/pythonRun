4e96e22af5531570a3087b82a854a474036fe895
sklee <hadesleeq@gmail.com>
2018-09-13 10:03:20 +0900

    함수의 반환값

---
+++ chapter10/sec02.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+# UTF-8 encoding when using korean
+
+a=8
+def mul_two():
+    global a
+    a = a * 2
+    return a
+
+mul_two()
+print(a)
+
+
+def example(a):
+    a = 6
+
+example(7)
+print(a)
