5e7245d97980012141f00d884614a3672eae9ff9
sklee <hadesleeq@gmail.com>
2018-09-13 09:58:46 +0900

    함수의 구조

--- chapter10/sec01.py
+++ chapter10/sec01.py
@@ -4,6 +4,18 @@
 def mul(a, b):
     return a * b
 
-
 c = mul(2, 3)
 print(c)
+
+def returnHI():
+    return 'HI!'
+
+def sayHi(signal):
+    if(sayHi==1):
+        print("Hi!")
+    else:
+        print("Bye!")
+
+def sayBye():
+    print("Bye!")
+
