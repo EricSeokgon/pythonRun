b69e172961a3941be3dc96f1d725cce114e01ac8
hadesyi <leesk55@gmail.com>
2018-09-16 03:41:10 +0900

    클래스

---
+++ chapter12/sec01.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+# UTF-8 encoding when using korean
+
+# 자동차1
+car_brand1 = "Toyota"
+car_color1 = "red"
+car_year1 = 1996
+
+car_brand2 = "Hyendi"
+car_color2 = "blue"
+car_year2 = 2009
+
+car_brand3 = "BMW"
+car_color3 = "black"
+car_year3 = 2000
