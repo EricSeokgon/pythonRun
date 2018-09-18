adfb39411820289fddf3b73aaa13a9a36a0e3e03
sklee <hadesleeq@gmail.com>
2018-09-18 10:04:15 +0900

    모듈 만들기1

---
+++ chapter13/calculator.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+# UTF-8 encoding when using korean
+
+def add(a, b):
+    return a + b
+
+
+def sub(a, b):
+    return a - b
+
+
+def mul(a, b):
+    return a * b
+
+
+def div(a, b):
+    return a / b
+
+
+def mod(a, b):
+    return a % b
+
