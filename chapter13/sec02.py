3ce44c47f3559a8f369d2b4aaa52343eafa326de
sklee <hadesleeq@gmail.com>
2018-09-18 10:08:05 +0900

    모듈 만들기1

---
+++ chapter13/sec02.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+# UTF-8 encoding when using korean
+import calculator
+
+add_result = calculator.add(10,2)
+sub_result = calculator.sub(10,2)
+mul_result = calculator.mul(10,2)
+div_result = calculator.div(10,2)
+mod_result = calculator.mod(10,2)
+
+print(add_result)
+print(sub_result)
+print(mul_result)
+print(div_result)
+print(mod_result)
+
